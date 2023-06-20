2d65f821bd9dc29ca8723689c89f2b8da1e39fa3
Amine Amatoury <aminov.02@hotmail.com>
2023-06-20 22:58:44 +0300

    CORS fix 3.0

--- src/main/java/ru/tsu/hits/userservice/configuration/SecurityConfigurer.java
+++ src/main/java/ru/tsu/hits/userservice/configuration/SecurityConfigurer.java
@@ -3,6 +3,7 @@ package ru.tsu.hits.userservice.configuration;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -36,6 +37,7 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .authorizeRequests().antMatchers("/authenticate", "/users/sign-up", "/swagger-ui/**", "/v3/api-docs/**", "/swagger-ui.html", "/webjars/**", "/v2/**", "/swagger-resources/**").permitAll()
+                .antMatchers(HttpMethod.OPTIONS,"/**").permitAll() // allow CORS preflight requests
                 .anyRequest().authenticated()
                 .and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
